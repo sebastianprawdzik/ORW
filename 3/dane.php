826d967487b4361306c039516121f267bbc4404d
Sebastian Prawdzik <sebastian_1@poczta.onet.pl>
2016-01-14 11:26:17 +0100

    df
    1

---
+++ 3/dane.php
@@ -0,0 +1,20 @@
+﻿<?php
+
+require('simple_html_dom.php');
+
+$html = file_get_html("http://www.suwalki.info/");
+
+foreach($html->find('img') as $element) 
+       echo $element->src . '<br>';
+
+foreach($html->find('div[id=kino]') as $element) 
+       echo $element;
+	   
+foreach($html->find('div[id=sonda]') as $element) 
+       echo $element;
+	   
+	   //echo $html;
+
+
+?>
+
